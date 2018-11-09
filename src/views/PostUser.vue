517271b1739be7adfc4cdeabb68ab74a00660867
Botuca <botucacontato@gmail.com>
2018-11-09 18:35:17 -0200

    #9 - Criação da tela de cadastro de usuário

--- src/views/PostUser.vue
+++ src/views/PostUser.vue
@@ -1,31 +1,54 @@
 <template>
   <v-container fluid grid-list-md>
     <v-layout row wrap justify-center>
-      <v-flex xs12 md5>
+      <v-flex xs12 md6>
         <v-card>
           <v-form refs="form">
             <v-card-text>
-              <v-layout fill-height>
-                <v-flex xs12 align-end flexbox>
-                  <p><span class="headline">Cadastro de Usuário</span></p>
-                </v-flex>
-              </v-layout>
+              <p class="display-1">Cadastro de Usuário</p>
               <v-text-field
-                  label="Nome completo:"
-                  placeholder="Digite seu nome completo"
+                :error-messages="errors.collect('name')"
+                data-vv-as="nome"
+                data-vv-name="name"
+                label="Nome completo"
+                placeholder="Digite seu nome completo"
+                v-model="name"
+                v-validate="'required|min:3'"
               ></v-text-field>
               <v-text-field
-                  label="E-mail:"
-                  placeholder="Digite seu e-mail"
+                :error-messages="errors.collect('email')"
+                data-vv-as="email"
+                data-vv-name="email"
+                label="E-mail"
+                placeholder="Digite seu e-mail"
+                v-model="email"
+                v-validate="'required|email'"
               ></v-text-field>
               <v-text-field
-                  label="Senha:"
-                  placeholder="Digite sua senha"
+                :error-messages="errors.collect('password')"
+                data-vv-as="senha"
+                data-vv-name="password"
+                label="Senha"
+                placeholder="Digite sua senha"
+                type="password"
+                v-model="password"
+                v-validate="'required|min:8'"
               ></v-text-field>
               <v-text-field
-                  label="Confirmação de senha:"
-                  placeholder="Digite sua senha novamente"
+                :error-messages="errors.collect('confirmPassword')"
+                data-vv-as="confirmação de senha"
+                data-vv-name="confirmPassword"
+                label="Confirmação de senha"
+                placeholder="Digite sua senha novamente"
+                type="password"
+                v-model="confirmPassword"
+                v-validate="{ is: password, required: true }"
               ></v-text-field>
+              <v-card-actions>
+                <v-btn color="info" @click="submit">
+                  Criar conta
+                </v-btn>
+              </v-card-actions>
             </v-card-text>
           </v-form>
         </v-card>
@@ -33,3 +56,23 @@
     </v-layout>
   </v-container>
 </template>
+
+<script>
+export default {
+  name: 'PostUser',
+  data() {
+    return {
+      confirmPassword: null,
+      email: null,
+      name: null,
+      password: null,
+    };
+  },
+  methods: {
+    submit() {
+      this.$validator.validateAll();
+    },
+  },
+};
+
+</script>
