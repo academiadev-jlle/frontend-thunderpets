356a52331a488d504f59b498380573a5c52a1cfd
Botuca <botucacontato@gmail.com>
2018-11-09 16:15:18 -0200

    adição do campo de nome, email, senha e confirmação de senha.

--- src/views/PostUser.vue
+++ src/views/PostUser.vue
@@ -1,29 +1,32 @@
 <template>
-  <v-container id="container">
-    <v-layout row wrap>
+  <v-container fluid grid-list-md>
+    <v-layout row wrap justify-center>
       <v-flex xs12 md5>
         <v-card>
           <v-form refs="form">
-          <v-card-text>
-        
-            <v-layout fill-height>
-              <v-flex xs12 align-end flexbox>
-                <p><span class="headline">Cadastro de Usuário</span></p>
-              </v-flex>
-            </v-layout>
-          
-            <v-text-field
-                label="Nome de usuário:"
-                placeholder="Insira um apelido "
-            ></v-text-field>
-
-            <v-textarea
-              label="Descrição"
-              no-resize
-              placeholder="Descreva o animal em poucas palavras para que possam reconhecê-lo"
-            ></v-textarea>
-
-          </v-card-text>
+            <v-card-text>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <p><span class="headline">Cadastro de Usuário</span></p>
+                </v-flex>
+              </v-layout>
+              <v-text-field
+                  label="Nome completo:"
+                  placeholder="Digite seu nome completo"
+              ></v-text-field>
+              <v-text-field
+                  label="E-mail:"
+                  placeholder="Digite seu e-mail"
+              ></v-text-field>
+              <v-text-field
+                  label="Senha:"
+                  placeholder="Digite sua senha"
+              ></v-text-field>
+              <v-text-field
+                  label="Confirmação de senha:"
+                  placeholder="Digite sua senha novamente"
+              ></v-text-field>
+            </v-card-text>
           </v-form>
         </v-card>
       </v-flex>
