47e98601477d4cef1ed9749eafd7b05ddbab6524
Botuca <botucacontato@gmail.com>
2018-11-27 16:58:45 -0200

    Parte de cadastro de usuário colocado lado a lado com o cadastro de contatos

--- src/views/PostUser.vue
+++ src/views/PostUser.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container fluid grid-list-md>
-    <v-layout row wrap justify-center>
-      <v-flex xs1 md6>
-        <v-card>
+    <v-card>
+      <v-layout row wrap justify-center>
+        <v-flex xs12 md6>
           <v-form refs="form">
             <v-card-text>
               <p class="display-1">Cadastro de Usuário</p>
@@ -44,83 +44,84 @@
                 v-model="confirmPassword"
                 v-validate="{ is: password, required: true }"
               ></v-text-field>
-              <v-text-field
-                label="Contato"
-                placeholder="Digite seu meio de contato"
-              ></v-text-field>
-              <v-layout row wrap>
-                <v-flex xs12 md4>
-                  <v-select
-                    :items="types"
-                    label="Tipo de Contato"
-                    placeholder="Selecione o tipo de contato"
-                    v-model="contacts.type"
-                  ></v-select>
-                </v-flex>
-                <v-flex xs12 md4>
-                  <v-text-field
-                    label="Contato"
-                    placeholder="Digite aqui seu contato"
-                    v-model="contacts.description"
-                  ></v-text-field>
-                </v-flex>
-                <v-flex xs12 md4>
-                  <v-btn
-                    color="success"
-                    @click="addRow"
-                  >
-                    Adicionar
-                  </v-btn>
-                </v-flex>
-              </v-layout>
-              <v-layout row>
-                <v-flex xs12 sm6 offset-sm3>
-                  <v-card>
-                    <v-toolbar color="primary">
-                      <v-toolbar-title>Seus contatos</v-toolbar-title>
-                      <v-spacer></v-spacer>
-                    </v-toolbar>
-                    <v-list two-line>
-                      <template v-for="contact in contacts">
-                        <v-list-tile
-                          :key="contact.name"
-                        >
-                          <v-list-tile-content>
-                            <v-list-tile-title default>
-                              {{ contact.type }}</v-list-tile-title>
-                            <v-list-tile-sub-title
-                              class="text--primary"
-                            >
-                              {{ contact.description }}
-                            </v-list-tile-sub-title>
-                          </v-list-tile-content>
-                          <v-btn
-                            flat
-                            icon color="red lighten-2"
-                            @click="deleteRow"
-                          >
-                            <v-icon>delete</v-icon>
-                          </v-btn>
-                        </v-list-tile>
-                      </template>
-                    </v-list>
-                  </v-card>
-                </v-flex>
-              </v-layout>
             </v-card-text>
-            <v-card-actions>
-              <v-btn
-                color="info"
-                @click="submit"
-                block
-              >
-                Criar conta
-              </v-btn>
-            </v-card-actions>
           </v-form>
-        </v-card>
-      </v-flex>
-    </v-layout>
+        </v-flex>
+        <v-flex xs12 md6>
+          <v-card-text>
+            <p class="display-1">Contatos do Usuário</p>
+            <v-layout row wrap>
+              <v-flex xs12 md4>
+                <v-select
+                  :items="types"
+                  label="Tipo de Contato"
+                  placeholder="Selecione o tipo de contato"
+                  v-model="contacts.type"
+                ></v-select>
+              </v-flex>
+              <v-flex xs12 md4>
+                <v-text-field
+                  label="Contato"
+                  placeholder="Digite aqui seu contato"
+                  v-model="contacts.description"
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12 md4>
+                <v-btn
+                  color="success"
+                  @click="addRow"
+                >
+                  Adicionar
+                </v-btn>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs12 sm6 offset-sm3>
+                <v-card>
+                  <v-toolbar color="primary">
+                    <v-toolbar-title>Seus contatos</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                  </v-toolbar>
+                  <v-list two-line>
+                    <template v-for="contact in contacts">
+                      <v-list-tile
+                        :key="contact.name"
+                      >
+                        <v-list-tile-content>
+                          <v-list-tile-title default>
+                            {{ contact.type }}</v-list-tile-title>
+                          <v-list-tile-sub-title
+                            class="text--primary"
+                          >
+                            {{ contact.description }}
+                          </v-list-tile-sub-title>
+                        </v-list-tile-content>
+                        <v-btn
+                          flat
+                          icon color="red lighten-2"
+                          @click="deleteRow"
+                        >
+                          <v-icon>delete</v-icon>
+                        </v-btn>
+                      </v-list-tile>
+                    </template>
+                  </v-list>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+        </v-flex>
+      </v-layout>
+      <v-card-actions>
+        <v-btn
+          color="info"
+          @click="submit"
+          block
+        >
+          Criar conta
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 
@@ -146,11 +147,9 @@ export default {
         description: this.contacts.description,
         type: this.contacts.type,
       });
-      console.log(this.contacts);
     },
     deleteRow() {
       this.contacts.splice(this.index, 1);
-      console.log(this.contacts);
     },
   },
 };
